713d506bb4d5518f398d0a47e73d3bab75c188f7
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-01-21 11:25:17 +0530

    Update DataNav.jsx

--- src/components/Nav/DataNav.jsx
+++ src/components/Nav/DataNav.jsx
@@ -26,4 +26,44 @@ const MainNavData = [
     },        
 ]
 
+const secNavData = [
+    {
+        id: 1,
+        name: 'About',
+        submenu: [
+            {
+                id: 1,
+                name: 'The University',
+                menusubL: [
+                    { id: 1, link: '',  name: 'History & Heritage'},
+                    { id: 2, link: '',  name: 'University Crest'},
+                    { id: 3, link: '',  name: 'Vision & Mission'},
+                    { id: 4, link: '',  name: 'University Orgnization'},
+                    { id: 5, link: '',  name: 'University Ranking'},
+                    { id: 6, link: '',  name: 'Generak Convocation'},
+                    { id: 7, link: '',  name: 'University Calendar Dates'},
+                    { id: 8, link: '',  name: 'Information Officers'},
+                ]
+            },
+            {
+                id: 2,
+                name: 'Leadership & Governance',
+                menusubL: [
+                    { id: 1, link: '',  name: 'University Organization'},
+                    { id: 2, link: '',  name: 'Officers'},
+                    { id: 3, link: '',  name: 'Council'},
+                    { id: 4, link: '',  name: 'Senate'},
+                    { id: 5, link: '',  name: 'Chancellor'},
+                    { id: 6, link: '',  name: 'Vice-Chancellor'},
+                    { id: 7, link: '',  name: 'Deputy Vice-Chancellor'},
+                    { id: 8, link: '',  name: 'Deans of the Faculties'},
+                    { id: 9, link: '',  name: 'Librarian'},
+                    { id: 10, link: '',  name: 'Registrar'},
+                    { id: 11, link: '',  name: 'Bursar'},
+                ]
+            }
+        ]
+    }
+]
+
 export default MainNavData
