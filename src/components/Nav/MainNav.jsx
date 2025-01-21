77aad2f91f21948fb8f2c5d697814d3b0405114c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-01-21 10:29:16 +0530

    Update

--- src/components/Nav/MainNav.jsx
+++ src/components/Nav/MainNav.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import UopLogo from '../../assets/uoplogo.png'
+import MainNavData from './DataNav'
 
 const MainNav = () => {
   return (
-    <div>MainNav</div>
+    <div className='md:px-28 px-4 py-4'>        
+        <div className="flex justify-between">
+            <div className="">
+                <img src={UopLogo} alt="" className='h-20 w-auto'/>
+            </div>
+            <div className="">
+                <div className="md:block hidden">
+                    <div className="flex">
+                        {
+                            MainNavData.map((item, index) => {
+                                return(
+                                    <div className="" key={index}>
+                                        {item.name}
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
