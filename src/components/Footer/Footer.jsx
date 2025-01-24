8523c1a2be4cdeb37462df41c20789507d1550dc
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-01-24 19:45:59 +0530

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,39 @@
 import React from 'react'
+import UopLogo from '../../assets/uoplogo.png'
 
 const Footer = () => {
   return (
-    <div className='md:px-28 px-4 bg-[#560606] text-white py-16'>
-        <div className="md:flex">
-            <div className="md:w-1/3">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit atque nisi porro assumenda exercitationem nulla voluptates, error debitis quis sequi eos sint dolore ipsam consequatur similique iure aliquid repellat illum?
-            </div>
-            <div className="md:w-2/3">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit atque nisi porro assumenda exercitationem nulla voluptates, error debitis quis sequi eos sint dolore ipsam consequatur similique iure aliquid repellat illum?
+    <div className="bg-[#560606]">
+        <div className='md:px-28 px-4 text-white py-16'>
+            <div className="md:flex">
+                <div className="md:w-1/3">
+                    <img src={UopLogo} alt="" className='h-20 w-auto'/>
+
+                    <div className="mt-4 ml-8">
+                        <p className="py-2 text-lg">University of Peradeniya</p>
+                        <p className="py-2 text-lg">Peradeniya</p>
+                        <p className="py-2 text-lg">20400</p>
+                        <p className="py-2 text-lg">Sri Lanka</p>
+                    </div>
+
+                    <div className="mt-4 ml-8">
+                        <p className="">Phone : <span>+94 81 238 8301</span></p>
+                        <p className="">Fax : <span>+94 81 238 8102</span></p>
+                    </div>
+                </div>
+                <div className="md:w-2/3">
+                    <div className="md:grid grid-cols-3 gap-4">
+                        <div className="w-full">
+                            <h1 className="uppercase font-semibold text-xl">main navigation</h1>
+                        </div>
+                        <div className="w-full">
+                            <h1 className="uppercase font-semibold text-xl">Information for</h1>
+                        </div>
+                        <div className="w-full">
+                            <h1 className="uppercase font-semibold text-xl">quick links</h1>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
