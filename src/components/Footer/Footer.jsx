5dc6e9718bbd2e3c93a1e2409beaed3fbee86f0f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-01-24 19:23:08 +0530

    Update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='md:px-28 px-4 bg-[#560606] text-white py-16'>
+        <div className="md:flex">
+            <div className="md:w-1/3">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit atque nisi porro assumenda exercitationem nulla voluptates, error debitis quis sequi eos sint dolore ipsam consequatur similique iure aliquid repellat illum?
+            </div>
+            <div className="md:w-2/3">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit atque nisi porro assumenda exercitationem nulla voluptates, error debitis quis sequi eos sint dolore ipsam consequatur similique iure aliquid repellat illum?
+            </div>
+        </div>
+    </div>
   )
 }
 
