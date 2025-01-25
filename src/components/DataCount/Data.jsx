9567a115a527a186d41d4ce7c8c50b9f39ef4975
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-01-25 07:15:29 +0530

    Update

---
+++ src/components/DataCount/Data.jsx
@@ -0,0 +1,34 @@
+const dataforcount = [
+    {
+        id: 1,
+        name: 'Internal Students',
+        value: 2000,
+        icon: '',
+    },
+    {
+        id: 2,
+        name: 'External Students',
+        value: 2000,
+        icon: '',
+    },
+    {
+        id: 3,
+        name: 'Faculties',
+        value: 9,
+        icon: '',
+    },
+    {
+        id: 4,
+        name: 'Academic Staff',
+        value: 150,
+        icon: '',
+    },
+    {
+        id: 5,
+        name: 'Administrative Staff',
+        value: '',
+        icon: '',
+    },
+]
+
+export { dataforcount }
---
+++ src/components/DataCount/DataCount.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DataCount = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default DataCount
