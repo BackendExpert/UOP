fbc332a706d0999691a439b9b60707b2e6c98ed3
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-01-25 09:32:40 +0530

    Update ProgramData.jsx

--- src/components/ProgramData/ProgramData.jsx
+++ src/components/ProgramData/ProgramData.jsx
@@ -1,5 +1,74 @@
 const programData = [
-    
+    {
+        id: 1,
+        name: 'Postgraduate Programs',
+        desc: 'Postgraduate Programs of University of Peradeniya',
+        courses: [
+            {
+                id: 1,
+                name: 'Postgraduate Programs in Faculty of Management',
+                link: '#',
+            },
+            {
+                id: 2,
+                name: 'Postgraduate Institute of Agriculture (PGIA)',
+                link: '#',
+            },
+            {
+                id: 3,
+                name: 'Postgraduate Institute of Humanities and Social Sciences (PGIHS)',
+                link: '#',
+            },
+            {
+                id: 4,
+                name: 'Postgraduate Institute of Medical Science (PGIMS)',
+                link: '#',
+            },
+            {
+                id: 5,
+                name: 'Postgraduate Institute of Science (PGIS)',
+                link: '#',
+            },
+            {
+                id: 6,
+                name: 'MSc. in Nursing from Faculty of Allied Health Sciences',
+                link: '#',
+            },
+            {
+                id: 7,
+                name: 'MSc in Energy for Circular Economy',
+                link: '#',
+            }
+        ]
+    },
+    {
+        id: 2,
+        name: 'Distance Education',
+        desc: 'The University of Peradeniya is the pioneering institution that initiated offering external degrees in the country since 1964.',
+        courses: [
+            {
+                id: 1,
+                name: 'Degree Programs',
+                link: '#'
+            },
+            {
+                id: 2,
+                name: 'Diploma Programs',
+                link: '#'
+            },
+            {
+                id: 3,
+                name: 'Certificate Programs',
+                link: '#'
+            },
+            {
+                id: 4,
+                name: 'Short Courses',
+                link: '#'
+            },
+            
+        ]
+    }
 ]
 
 export { programData }
