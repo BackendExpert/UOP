88fe0a30be0c3995d250172f27320c8b411ed87a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-01-26 09:15:09 +0530

    Update

--- src/components/SGData/SGData.jsx
+++ src/components/SGData/SGData.jsx
@@ -1,5 +1,16 @@
 import React from 'react'
 
+const today = new Date();
+const date = today.getDate()
+const day = today.getDay();
+const month = today.getMonth()
+const year = today.getFullYear();
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const monthNames = [
+    "January", "February", "March", "April", "May", "June", 
+    "July", "August", "September", "October", "November", "December"
+];
+
 const JanNewYear = () => {
     const currentYear = new Date().getFullYear();
   return (
@@ -9,6 +20,7 @@ const JanNewYear = () => {
             <h1 className="py-2 text-2xl sg-font">A new year is a blank page, write your best story yet</h1>
             <h1 className="py-2 text-2xl">May this year be filled with new adventures and academic achievements</h1>
             <h1 className="uppercase sg-font py-2">Happy New Year {currentYear}</h1>
+            <h1 className="uppercase py-2">{date} {days[day]} , {monthNames[month]} {year}</h1>
         </div>
     </div>
   )
@@ -23,6 +35,7 @@ const Aprilfunction = () => {
             <h1 className="py-2 text-2xl sg-font">Have an Enjoyable New Year !!!</h1>
             <h1 className="py-2 text-2xl">Wishing you a very Happy & Blessed Sinhala & Tamil New Year, May this New Year bring you much Happiness & Prosperity</h1>
             <h1 className="uppercase sg-font py-2">{currentYear}</h1>
+            <h1 className="uppercase py-2">{date} {days[day]} , {monthNames[month]} {year}</h1>
         </div>
     </div>
     )
@@ -37,6 +50,7 @@ const WasakFunction = () => {
             <h1 className="py-2 text-2xl sg-font">Happy Vesak Festival !!!</h1>
             <h1 className="py-2 text-2xl">"Do not dwell in the past, do not dream of the future, concentrate the mind on the present moment."</h1>
             <h1 className="uppercase sg-font py-2">{currentYear}</h1>
+            <h1 className="uppercase py-2">{date} {days[day]} , {monthNames[month]} {year}</h1>
         </div>
     </div>
     )
@@ -51,6 +65,7 @@ const AssalaPerahera = () => {
             <h1 className="py-2 text-2xl sg-font">Wishing you blessings during this sacred Esala season.!!!</h1>
             <h1 className="py-2 text-2xl">The Kandy Perahera, where devotion meets celebration in the heart of Sri Lanka’s sacred city.</h1>
             <h1 className="uppercase sg-font py-2">{currentYear}</h1>
+            <h1 className="uppercase py-2">{date} {days[day]} , {monthNames[month]} {year}</h1>
         </div>
     </div>
     )
@@ -65,9 +80,24 @@ const MarryCrismistmas = () => {
             <h1 className="py-2 text-2xl sg-font">Merry Christmas</h1>
             <h1 className="py-2 text-2xl">Merry Christmas! May your season be filled with joy, peace, and love.</h1>
             <h1 className="uppercase sg-font py-2">{currentYear}</h1>
+            <h1 className="uppercase py-2">{date} {days[day]} , {monthNames[month]} {year}</h1>
         </div>
     </div>
     )
 }
 
-export { JanNewYear, Aprilfunction, WasakFunction, AssalaPerahera, MarryCrismistmas }
+const DefultSG = () => {
+
+    return (
+    <div className='relative bg-[url(https://wallpapercave.com/wp/wp12885071.jpg)] bg-cover bg-center py-32 text-center text-white'>
+        <div className="absolute inset-0 bg-black opacity-60"></div> 
+        <div className="relative">
+            <h1 className="py-2 text-2xl sg-font">Have Nice Day</h1>
+            <h1 className="py-2 text-2xl">Never give up. Today is hard, tomorrow will be worse, but the day after tomorrow will be sunshine.</h1>
+            <h1 className="uppercase py-2">{date} {days[day]} , {monthNames[month]} {year}</h1>
+        </div>
+    </div>
+    )
+}
+
+export { JanNewYear, Aprilfunction, WasakFunction, AssalaPerahera, MarryCrismistmas, DefultSG}
--- src/pages/SeasonsGreetings/SeasonsGreetings.jsx
+++ src/pages/SeasonsGreetings/SeasonsGreetings.jsx
@@ -1,14 +1,52 @@
 import React from 'react'
-import { JanNewYear, Aprilfunction, WasakFunction, AssalaPerahera, MarryCrismistmas } from '../../components/SGData/SGData'
+import { JanNewYear, Aprilfunction, WasakFunction, AssalaPerahera, MarryCrismistmas, DefultSG } from '../../components/SGData/SGData'
 
 const SeasonsGreetings = () => {
   return (
     <div className="">
-        <JanNewYear />
-        <Aprilfunction />
-        <WasakFunction />
-        <AssalaPerahera />
-        <MarryCrismistmas />        
+        <div className="">
+            <JanNewYear />
+            <Aprilfunction />
+            <WasakFunction />
+            <AssalaPerahera />
+            <MarryCrismistmas />  
+        </div>
+        {/* <div className="">
+            {
+                (() => {
+                    if(new Date().getMonth() === 0){
+                        return (
+                            <JanNewYear />
+                        )
+                    }
+                    else if(new Date().getMonth() === 3){
+                        return (
+                            <Aprilfunction />
+                        )
+                    }
+                    else if(new Date().getMonth() === 4){
+                        return (
+                            <WasakFunction />
+                        )   
+                    }
+                    else if(new Date().getMonth() === 7){
+                        return (
+                            <AssalaPerahera />
+                        )
+                    }
+                    else if(new Date().getMonth() === 11){
+                        return (
+                            <MarryCrismistmas />
+                        )
+                    }
+                    else{
+                        return (
+                            <DefultSG />
+                        )
+                    }
+                })()
+            }
+        </div> */}
     </div>
   )
 }
