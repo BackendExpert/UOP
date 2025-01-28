e52317d818b3d9fff3bc5e85151a07be2c663c8d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-01-28 12:30:11 +0530

    Update Events.jsx

--- src/pages/Events/Events.jsx
+++ src/pages/Events/Events.jsx
@@ -6,13 +6,24 @@ const Events = () => {
     <div>
         <h1 className="text-center text-2xl font-semibold uppercase text-[#560606]">Events</h1>
 
-        <div className="bg-white my-4">
+        <div className="my-4">
             {
                 testeventdata.map((event, index) => {
                     return (
-                        <div className="border-b border-gray-200 p-4" key={index}>
+                        <div data-aos="zoom-in" className="mb-4 rounded shadow-xl bg-white p-4" key={index}>
                             <h1 className="text-xl font-semibold text-[#560606]">{event.name}</h1>
                             <p className="pt-2">{event.decs}</p>
+
+                            <div className="flex justify-between pt-4">
+                                <div className="">
+                                    <a href={event.link}>
+                                        <h1 className="text-[#e8b910] font-semibold hover:text-[#560606] duration-500">Read More</h1>
+                                    </a>
+                                </div>
+                                <div className="">
+                                    <h1 className="text-gray-500">{event.date}</h1>
+                                </div>
+                            </div>
                         </div>
                     )
                 })
