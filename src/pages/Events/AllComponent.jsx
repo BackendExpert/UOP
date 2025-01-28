1808cfcf9a07b3f25fe9c8c2e191efe27321fa88
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-01-28 12:26:16 +0530

    Update

--- src/pages/Events/AllComponent.jsx
+++ src/pages/Events/AllComponent.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import Events from './Events'
+import QuickLinks from './QuickLinks'
+import Notice from './Notice'
 
 const AllComponent = () => {
   return (
-    <div className='my-4 md:px-28 px-4'>AllComponent</div>
+    <div className='my-4 md:px-28 px-4 bg-gray-200 py-16'>
+        <div className="md:flex">
+            <div className="w-1/3">
+                <Events />
+            </div>
+            <div className="w-1/3">
+                <QuickLinks />
+            </div>
+            <div className="w-1/3">
+                <Notice />
+            </div>
+        </div>
+    </div>
   )
 }
 
---
+++ src/pages/Events/QuickLinks.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const QuickLinks = () => {
+  return (
+    <div>
+        <h1 className="text-center text-2xl font-semibold uppercase text-[#560606]">Quick Links</h1>
+    </div>
+  )
+}
+
+export default QuickLinks
