40b9e365e6ecd6216aeecf4d14a63b21e4bf3e8c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-01-29 13:26:27 +0530

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -63,10 +63,32 @@ const Footer = () => {
                                     }
                                 </div>
                             </div>
-                            <div className="mt-4">
-                                <h1 className="uppercase font-semibold text-xl">Get in Touch</h1>
-                                <hr className='w-3/4 my-2'/>
-                                <div className="grid grid-cols-2 gap-4 mt-4">
+                        </div>
+                        <div className="w-full md:my-0 my-8">
+                            <h1 className="uppercase font-semibold text-xl">location</h1>
+                            <hr className='w-3/4 my-2'/>
+                            
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3957.8760289680417!2d80.59481527558846!3d7.254947514230262!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae368deaaaaaaab%3A0xb5476b88282d47fb!2sUniversity%20of%20Peradeniya!5e0!3m2!1sen!2slk!4v1738049825365!5m2!1sen!2slk" 
+                                className='h-80 w-auto mt-4'
+                                style={{ border: '1px'}} 
+                                allowfullscreen="" 
+                                loading="lazy" 
+                                referrerpolicy="no-referrer-when-downgrade">
+                            </iframe>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div className="py-2 px-4 border-t border-[#e8b910] md:text-left text-center">            
+            <div className="md:flex justify-between">
+                <div className="">
+                    Copyright © {currentYear} University of Peradeniya . All rights reserved.
+                </div>
+
+                <div className="flex">
                                         <a href="#" className='duration-500 hover:text-[#e8b910]'>
                                             <div className="flex">
                                                 <p className="px-2"><img src={FB} alt="" className='h-8 w-auto'/></p>
@@ -98,33 +120,7 @@ const Footer = () => {
                                                 <h1 className="">Linkedin</h1>
                                             </div>
                                         </a>
-
                                 </div>
-                            </div>
-                        </div>
-                        <div className="w-full md:my-0 my-8">
-                            <h1 className="uppercase font-semibold text-xl">location</h1>
-                            <hr className='w-3/4 my-2'/>
-                            
-                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3957.8760289680417!2d80.59481527558846!3d7.254947514230262!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae368deaaaaaaab%3A0xb5476b88282d47fb!2sUniversity%20of%20Peradeniya!5e0!3m2!1sen!2slk!4v1738049825365!5m2!1sen!2slk" 
-                                className='h-80 w-auto mt-4'
-                                style={{ border: '1px'}} 
-                                allowfullscreen="" 
-                                loading="lazy" 
-                                referrerpolicy="no-referrer-when-downgrade">
-                            </iframe>
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div className="py-2 px-4 border-t border-[#e8b910] md:text-left text-center">            
-            <div className="md:flex justify-between">
-                <div className="">
-                    Copyright © {currentYear} University of Peradeniya . All rights reserved.
-                </div>
 
             </div>
         </div>
