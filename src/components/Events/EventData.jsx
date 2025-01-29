10a4aa7b00185bad558ee19ab94215ffea082a22
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-01-29 11:18:58 +0530

    Update EventData.jsx

--- src/components/Events/EventData.jsx
+++ src/components/Events/EventData.jsx
@@ -1,7 +1,7 @@
 const testeventdata = [
     {
         id: 1,
-        img: 'https://wallpapercave.com/wp/wp11704432.jpg',
+        img: 'https://wallpapercave.com/wp/wp9452673.jpg',
         name: 'Event 1',
         decs: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores voluptate eveniet, repellat quod quia optio laboriosam hic quos ducimus reiciendis consequuntur expedita eaque possimus inventore deleniti, sed tenetur! Voluptatem, ipsam!',
         link: '#',
@@ -9,7 +9,7 @@ const testeventdata = [
     },
     {
         id: 2,
-        img: 'https://wallpapercave.com/wp/wp11704432.jpg',
+        img: 'https://wallpapercave.com/wp/wp12681972.jpg',
         name: 'Event 1',
         decs: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores voluptate eveniet, repellat quod quia optio laboriosam hic quos ducimus reiciendis consequuntur expedita eaque possimus inventore deleniti, sed tenetur! Voluptatem, ipsam!',
         link: '#',
@@ -17,7 +17,7 @@ const testeventdata = [
     },
     {
         id: 3,
-        img: 'https://wallpapercave.com/wp/wp11704432.jpg',
+        img: 'https://wallpapercave.com/wp/wp14925950.jpg',
         name: 'Event 1',
         decs: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores voluptate eveniet, repellat quod quia optio laboriosam hic quos ducimus reiciendis consequuntur expedita eaque possimus inventore deleniti, sed tenetur! Voluptatem, ipsam!',
         link: '#',
@@ -25,7 +25,7 @@ const testeventdata = [
     },
     {
         id: 4,
-        img: 'https://wallpapercave.com/wp/wp11704432.jpg',
+        img: 'https://wallpapercave.com/wp/wp1895412.jpg',
         name: 'Event 1',
         decs: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores voluptate eveniet, repellat quod quia optio laboriosam hic quos ducimus reiciendis consequuntur expedita eaque possimus inventore deleniti, sed tenetur! Voluptatem, ipsam!',
         link: '#',
