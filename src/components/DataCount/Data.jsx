fdb932c288c01232db64691851bae4abbe036f7a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-01-30 09:45:07 +0530

    Update

--- src/components/DataCount/Data.jsx
+++ src/components/DataCount/Data.jsx
@@ -2,7 +2,6 @@ import { FaUserGraduate } from "react-icons/fa";
 import { FaSchool, FaUserTie, FaUserLock } from "react-icons/fa6";
 import { IoIosSchool } from "react-icons/io";
 
-
 const dataforcount = [
     {
         id: 1,
@@ -12,18 +11,24 @@ const dataforcount = [
     },
     {
         id: 2,
+        name: 'External Students',
+        value: 2000,
+        icon: IoIosSchool,
+    },
+    {
+        id: 3,
         name: 'Faculties',
         value: 9,
         icon: FaSchool,
     },
     {
-        id: 3,
+        id: 4,
         name: 'Academic Staff',
         value: 500,
         icon: FaUserTie,
     },
     {
-        id: 4,
+        id: 5,
         name: 'Administrative Staff',
         value: 1000,
         icon: FaUserLock,
