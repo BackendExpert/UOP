fb35dd99e7b93d6111bf9da3a05ab1dcce18252a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-02-09 20:25:52 +0530

    Update

---
+++ src/components/ReseachSection/ResearchAllData.jsx
@@ -0,0 +1,94 @@
+const allresearchdata = [
+    {
+        id: 1,
+        name: "Research 1",
+        img: 'https://wallpapercave.com/wp/wp4708110.jpg',
+        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus deleniti vero sapiente eaque rerum neque ipsa earum ut assumenda id modi corporis, numquam a ipsum! Minus perspiciatis ducimus adipisci voluptates.',
+        link: '#',
+        faculty: 'Faculty of Science',
+        facultyLink: '#'
+    },
+    {
+        id: 2,
+        name: "Research 2",
+        img: 'https://wallpapercave.com/wp/wp4708110.jpg',
+        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus deleniti vero sapiente eaque rerum neque ipsa earum ut assumenda id modi corporis, numquam a ipsum! Minus perspiciatis ducimus adipisci voluptates.',
+        link: '#',
+        faculty: 'Faculty of Science',
+        facultyLink: '#'
+    },
+    {
+        id: 3,
+        name: "Research 3",
+        img: 'https://wallpapercave.com/wp/wp4708110.jpg',
+        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus deleniti vero sapiente eaque rerum neque ipsa earum ut assumenda id modi corporis, numquam a ipsum! Minus perspiciatis ducimus adipisci voluptates.',
+        link: '#',
+        faculty: 'Faculty of Science',
+        facultyLink: '#'
+    },
+    {
+        id: 4,
+        name: "Research 4",
+        img: 'https://wallpapercave.com/wp/wp4708110.jpg',
+        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus deleniti vero sapiente eaque rerum neque ipsa earum ut assumenda id modi corporis, numquam a ipsum! Minus perspiciatis ducimus adipisci voluptates.',
+        link: '#',
+        faculty: 'Faculty of Science',
+        facultyLink: '#'
+    },
+    {
+        id: 5,
+        name: "Research 5",
+        img: 'https://wallpapercave.com/wp/wp4708110.jpg',
+        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus deleniti vero sapiente eaque rerum neque ipsa earum ut assumenda id modi corporis, numquam a ipsum! Minus perspiciatis ducimus adipisci voluptates.',
+        link: '#',
+        faculty: 'Faculty of Science',
+        facultyLink: '#'
+    },
+    {
+        id: 6,
+        name: "Research 6",
+        img: 'https://wallpapercave.com/wp/wp4708110.jpg',
+        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus deleniti vero sapiente eaque rerum neque ipsa earum ut assumenda id modi corporis, numquam a ipsum! Minus perspiciatis ducimus adipisci voluptates.',
+        link: '#',
+        faculty: 'Faculty of Science',
+        facultyLink: '#'
+    },
+    {
+        id: 7,
+        name: "Research 7",
+        img: 'https://wallpapercave.com/wp/wp4708110.jpg',
+        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus deleniti vero sapiente eaque rerum neque ipsa earum ut assumenda id modi corporis, numquam a ipsum! Minus perspiciatis ducimus adipisci voluptates.',
+        link: '#',
+        faculty: 'Faculty of Science',
+        facultyLink: '#'
+    },
+    {
+        id: 8,
+        name: "Research 8",
+        img: 'https://wallpapercave.com/wp/wp4708110.jpg',
+        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus deleniti vero sapiente eaque rerum neque ipsa earum ut assumenda id modi corporis, numquam a ipsum! Minus perspiciatis ducimus adipisci voluptates.',
+        link: '#',
+        faculty: 'Faculty of Science',
+        facultyLink: '#'
+    },
+    {
+        id: 9,
+        name: "Research 9",
+        img: 'https://wallpapercave.com/wp/wp4708110.jpg',
+        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus deleniti vero sapiente eaque rerum neque ipsa earum ut assumenda id modi corporis, numquam a ipsum! Minus perspiciatis ducimus adipisci voluptates.',
+        link: '#',
+        faculty: 'Faculty of Science',
+        facultyLink: '#'
+    },
+    {
+        id: 10,
+        name: "Research 10",
+        img: 'https://wallpapercave.com/wp/wp4708110.jpg',
+        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus deleniti vero sapiente eaque rerum neque ipsa earum ut assumenda id modi corporis, numquam a ipsum! Minus perspiciatis ducimus adipisci voluptates.',
+        link: '#',
+        faculty: 'Faculty of Science',
+        facultyLink: '#'
+    },
+]
+
+export {allresearchdata}
--- src/pages/RsearchAll/RsearchAll.jsx
+++ src/pages/RsearchAll/RsearchAll.jsx
@@ -1,24 +1,53 @@
 import React from 'react'
 import BarChart from '../../components/Charts/BarChart'
+import { allresearchdata } from '../../components/ReseachSection/ResearchAllData'
 
 const RsearchAll = () => {
   return (
-    <div className='xl:px-24 px-4 mt-16'>
+    <div className='xl:px-24 px-4 mt-16 bg-gray-200'>
         <div className="py-16">
             <h1 className="text-3xl uppercase font-semibold text-[#560606]">research and innovation</h1>
 
 
-            <div className="grid xl:grid-cols-3 gap-4 mt-8">
-                <div className="">
+            <div className="grid xl:grid-cols-3 md:grid-cols-2 gap-8 mt-8">
+                <div className="bg-white p-4 rounded-xl shadow-xl">
                     <h1 className="text-2xl font-semibold text-[#560606]">More Research</h1>
                     <p className='text-gray-500 mt-2'>
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet quisquam soluta aliquam voluptatum obcaecati magni minus cumque quam at placeat officiis tempora iure ad, eligendi et quidem iste, rem facere!
                     </p>
 
-                    <div className="md:block hidden">
+                    <div className="md:block hidden mt-4">
                         <BarChart />
                     </div>
                 </div>
+                {
+                    allresearchdata.map((research, index) => {
+                        return (
+                            <div className="max-h-full bg-white rounded-xl shadow-xl max-h-auto" key={index}>
+                                <img src={research.img} alt="" className='h-auto w-full rounded-t-xl'/>
+            
+                                <div className="p-4">
+                                    <h1 className="font-semibold uppercase text-[#560606] pb-2">{research.name}</h1>
+                                    <p className="text-gray-500">{research.desc}</p>
+            
+                                    <div className="mt-4 flex justify-between">
+                                        <div className="">
+                                            <a href={research.link}>
+                                                <button className='bg-[#560606] py-2 px-4 text-white rounded-full shadow-xl duration-500 hover:px-6'>Read More</button>
+                                            </a>
+                                        </div>
+                                        <div className="mt-2">
+                                            <a href={research.facultyLink}>
+                                                <p className="text-gray-500 duration-500 hover:text-[#560606] hover:font-semibold">{research.faculty}</p>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+                
             </div>
         </div>
     </div>
