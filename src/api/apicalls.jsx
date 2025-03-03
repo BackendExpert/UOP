0c8ef075f9d75674e21ddcf4e3dd49214c9b866f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-03-03 08:04:30 +0530

    Update apicalls.jsx

--- src/api/apicalls.jsx
+++ src/api/apicalls.jsx
@@ -1,6 +1,15 @@
 import axios from "axios";
 
-const Backendurl = "http://localhost/"
+const Backendurl = "http://localhost/uop_backend"
 
 const getToken = () => localStorage.getItem("token");
 
+export const loginuser = async(user) => {
+    const res = await axios.post(`${Backendurl}/functions/login.php`)
+    return res.data
+}
+
+export const signupuser = async(user) => {
+    const res = await axios.post(`${Backendurl}/functions/register.php`)
+    return res.data
+}
