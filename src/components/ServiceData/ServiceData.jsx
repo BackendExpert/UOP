6d4802dc40b350444002d865a379a65c7aae370c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-03-04 12:40:32 +0530

    Update ServiceData.jsx

--- src/components/ServiceData/ServiceData.jsx
+++ src/components/ServiceData/ServiceData.jsx
@@ -2,7 +2,7 @@ const serviceData = [
     {
         id: 1,
         name: 'Public Services',
-        link: '#',
+        link: '',
         datamenu: [
             {
                 id:1,
@@ -59,8 +59,59 @@ const serviceData = [
     {
         id: 2,
         name: 'Student Services',
-        link: '#',
-        datamenu: []
+        link: '',
+        datamenu: [
+            {
+                id:1,
+                name: 'Library Services',
+                link: '#',
+            },
+            {
+                id:2,
+                name: 'Guest House & Accommodation',
+                link: '#',
+            },
+            {
+                id:3,
+                name: 'Public Relations & Media',
+                link: '#',
+            },
+            {
+                id:4,
+                name: 'General Services',
+                link: '#',
+            },
+            {
+                id:5,
+                name: 'Event & Venue Booking',
+                link: '#',
+            },
+            {
+                id:6,
+                name: 'Health & Medical Services',
+                link: '#',
+            },
+            {
+                id:7,
+                name: 'University Tours',
+                link: '#',
+            },
+            {
+                id:8,
+                name: 'Research & Consultancy',
+                link: '#',
+            },
+            {
+                id:9,
+                name: 'Career & Job Fairs',
+                link: '#',
+            },
+            {
+                id:10,
+                name: 'IT & Digital Services',
+                link: '#',
+            },
+        ]
     },
     {
         id: 3,
