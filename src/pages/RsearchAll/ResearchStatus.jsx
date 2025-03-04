f601b415eb58765e6aa03aeb1fcbc77c613fabc9
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-03-04 13:57:42 +0530

    Upate

---
+++ src/pages/RsearchAll/ResearchStatus.jsx
@@ -0,0 +1,47 @@
+const researchstats = [
+    {
+        id: 1,
+        name: 'Journals',
+        icon: '',
+        link: '#'
+    },
+    {
+        id: 2,
+        name: 'iPURSE',
+        icon: '',
+        link: '#'
+    },
+    {
+        id: 3,
+        name: 'University Research Center',
+        icon: '',
+        link: '#'
+    },
+    {
+        id: 4,
+        name: 'Center for Research in Oral Cancer',
+        icon: '',
+        link: '#'
+    },
+    {
+        id: 5,
+        name: 'International Relations Office',
+        icon: '',
+        link: '#'
+    },
+    {
+        id: 6,
+        name: 'Multidisciplinary Research Centre (MDRC)',
+        icon: '',
+        link: '#'
+    },
+    {
+        id: 7,
+        name: 'Research Publications',
+        icon: '',
+        link: '#'
+    },
+S
+]
+
+export  { researchstats }
--- src/pages/RsearchAll/RsearchAll.jsx
+++ src/pages/RsearchAll/RsearchAll.jsx
@@ -9,8 +9,8 @@ const RsearchAll = () => {
             <h1 className="text-3xl uppercase font-semibold text-[#560606]">research and innovation</h1>
 
 
-            <div className="grid xl:grid-cols-3 md:grid-cols-2 gap-8 mt-8">
-                <div className="bg-white p-4 rounded-xl shadow-xl">
+            <div className="md:flex mt-4">
+                <div className="md:w-1/2 mr-2 bg-white p-4 rounded-xl shadow-xl">
                     <h1 className="text-2xl font-semibold text-[#560606]">More Research</h1>
                     <p className='text-gray-500 mt-2'>
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet quisquam soluta aliquam voluptatum obcaecati magni minus cumque quam at placeat officiis tempora iure ad, eligendi et quidem iste, rem facere!
@@ -20,6 +20,14 @@ const RsearchAll = () => {
                         <BarChart />
                     </div>
                 </div>
+                <div className="md:w-1/2 ml-2">
+                    
+                </div>
+            </div>
+
+
+
+            <div className="grid xl:grid-cols-3 md:grid-cols-2 gap-8 mt-8">
                 {
                     allresearchdata.map((research, index) => {
                         return (
