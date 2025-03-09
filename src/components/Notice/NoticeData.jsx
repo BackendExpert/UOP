b69411658d2c29b7e0ed4309c97813ea275360a8
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-03-09 19:58:21 +0530

    UPdate

--- src/components/Notice/NoticeData.jsx
+++ src/components/Notice/NoticeData.jsx
@@ -1,68 +1,29 @@
-const noticeData = [
-    {
-        id: 1,
-        name: "Notice 1",
-        desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores voluptate eveniet, repellat quod quia optio laboriosam hic quos ducimus reiciendis consequuntur expedita eaque possimus inventore deleniti, sed tenetur! Voluptatem, ipsam!',
-        link: '#',
-        date: '06 March 2025'
-    },
-    {
-        id: 2,
-        name: "Notice 2",
-        desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores voluptate eveniet, repellat quod quia optio laboriosam hic quos ducimus reiciendis consequuntur expedita eaque possimus inventore deleniti, sed tenetur! Voluptatem, ipsam!',
-        link: '#',
-        date: '06 March 2025'
-    },
-    {
-        id: 3,
-        name: "Notice 3",
-        desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores voluptate eveniet, repellat quod quia optio laboriosam hic quos ducimus reiciendis consequuntur expedita eaque possimus inventore deleniti, sed tenetur! Voluptatem, ipsam!',
-        link: '#',
-        date: '06 March 2025'
-    },
-    {
-        id: 4,
-        name: "Notice 1",
-        desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores voluptate eveniet, repellat quod quia optio laboriosam hic quos ducimus reiciendis consequuntur expedita eaque possimus inventore deleniti, sed tenetur! Voluptatem, ipsam!',
-        link: '#',
-        date: '06 March 2025'
-    },
-    {
-        id: 5,
-        name: "Notice 2",
-        desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores voluptate eveniet, repellat quod quia optio laboriosam hic quos ducimus reiciendis consequuntur expedita eaque possimus inventore deleniti, sed tenetur! Voluptatem, ipsam!',
-        link: '#',
-        date: '06 March 2025'
-    },
-    {
-        id: 6,
-        name: "Notice 3",
-        desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores voluptate eveniet, repellat quod quia optio laboriosam hic quos ducimus reiciendis consequuntur expedita eaque possimus inventore deleniti, sed tenetur! Voluptatem, ipsam!',
-        link: '#',
-        date: '06 March 2025'
-    },
-    {
-        id: 7,
-        name: "Notice 1",
-        desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores voluptate eveniet, repellat quod quia optio laboriosam hic quos ducimus reiciendis consequuntur expedita eaque possimus inventore deleniti, sed tenetur! Voluptatem, ipsam!',
-        link: '#',
-        date: '06 March 2025'
-    },
-    {
-        id: 8,
-        name: "Notice 2",
-        desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores voluptate eveniet, repellat quod quia optio laboriosam hic quos ducimus reiciendis consequuntur expedita eaque possimus inventore deleniti, sed tenetur! Voluptatem, ipsam!',
-        link: '#',
-        date: '06 March 2025'
-    },
-    {
-        id: 9,
-        name: "Notice 3",
-        desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores voluptate eveniet, repellat quod quia optio laboriosam hic quos ducimus reiciendis consequuntur expedita eaque possimus inventore deleniti, sed tenetur! Voluptatem, ipsam!',
-        link: '#',
-        date: '06 March 2025'
-    },
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-]
+const useNoticeData = () => {
+    const [noticedata, setnoticedata] = useState([]);
 
-export { noticeData }
+    useEffect(() => {
+        axios.get(import.meta.env.VITE_APP_API + "/notice.php", {
+            params: { action: "getallNotice" },
+            headers: { "Content-Type": "application/json" },
+        })
+        .then(res => {
+            console.log(res.data);
+            if (res.data.Result) {
+                setnoticedata(res.data.Result);
+            } else {
+                setnoticedata([]);
+            }
+        })
+        .catch(err => {
+            console.error(err);
+            setnoticedata([]);
+        });
+    }, []);
+
+    return noticedata;
+};
+
+export default useNoticeData;
