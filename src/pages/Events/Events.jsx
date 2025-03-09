caa1648e02577fc11f18a0f0c71599e3bc973f23
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-03-09 20:50:32 +0530

    Update

--- src/pages/Events/Events.jsx
+++ src/pages/Events/Events.jsx
@@ -1,20 +1,25 @@
 import React from 'react'
-import { testeventdata } from '../../components/Events/EventData'
+import useEventData from '../../components/Events/EventData'
 
 const Events = () => {
+    const eventdata = useEventData()
   return (
     <div>
         <h1 className="text-2xl font-semibold uppercase text-[#560606] mt-4">Events</h1>
 
         <div className="grid xl:grid-cols-3 md:grid-cols-2 gap-4">
             {
-                testeventdata.map((event, index) => {
-
+                eventdata.map((event, index) => {
+                    const date = new Date(event.event_date);
+                    const year = date.getFullYear();
+                    const month = date.toLocaleString("en-US", { month: "long" });
+                    const day = date.getDate().toString();
+                    const imageUrl = event.event_img 
                     return (
                         <div
                         key={index}
                         data-aos="zoom-in"
-                        style={{ backgroundImage: `url(${event.img})` }}
+                        style={{ backgroundImage: `url(${import.meta.env.VITE_APP_API}/${event.event_img})` }}
                         className={`
                           bg-cover bg-center h-auto my-4 shadow-xl rounded w-full relative bg-white py-32 group overflow-hidden
                           
@@ -32,17 +37,17 @@ const Events = () => {
                             <div className="absolute inset-0 bg-black opacity-40 group-hover:opacity-80 transition-opacity duration-500"></div>
                             <div className="absolute inset-0 flex items-center justify-center opacity-100 group-hover:opacity-0 transition-opacity duration-500">
                                 <div className="text-xs text-center p-2 rounded-md absolute top-2 right-4 text-white font-semibold bg-gray-500">
-                                    <h1 className="">{event.day.year}</h1>
-                                    <h1 className="">{event.day.month}</h1>
-                                    <h1 className="">{event.day.date}</h1>
+                                    <h1 className="">{year}</h1>
+                                    <h1 className="">{month}</h1>
+                                    <h1 className="">{day}</h1>
                                 </div>
-                                <h1 className="text-xl font-semibold text-white">{event.name}</h1>
+                                <h1 className="text-xl font-semibold text-white">{event.event_title}</h1>
                             </div>
                             <div className="p-4 absolute bg-[#e8b910] inset-0 flex items-center justify-center -translate-x-full opacity-0 group-hover:translate-x-0 group-hover:opacity-100 transition-all duration-500 ease-in-out">
                                 <div className="relative ">
-                                    <h1 className="text-xl text-[#560606] font-bold">{event.name}</h1>
-                                    <p className="text-[#560606]">{event.decs}</p>
-                                    <a href={event.link}>
+                                    <h1 className="text-xl text-[#560606] font-bold">{event.event_title}</h1>
+                                    <p className="text-[#560606]">{event.envet_desc}</p>
+                                    <a href={event.event_link}>
                                         <h1 className="text-[#560606] duration-500 hover:font-semibold">Read More</h1>
                                     </a>
                                 </div>
