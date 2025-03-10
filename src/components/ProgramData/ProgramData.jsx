d4eadc68205061dcf25a6a2b4b4d9aeee7df564a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-03-10 06:34:36 +0530

    Update

--- src/components/ProgramData/ProgramData.jsx
+++ src/components/ProgramData/ProgramData.jsx
@@ -25,7 +25,7 @@ const programData = [
             },
             {
                 id: 4,
-                name: 'Certificate Programs',
+                name: 'Short Courses',
                 img: 'https://wallpapercave.com/wp/wp1895394.jpg',
                 link: '#'
             },
@@ -39,13 +39,13 @@ const programData = [
         courses: [
             {
                 id: 1,
-                name: 'Postgraduate Programs in Faculty of Management',
+                name: 'Postgraduate Institute of Agriculture (PGIA)',
                 img: 'https://wallpapercave.com/wp/wp2351063.jpg',
                 link: '#',
             },
             {
                 id: 2,
-                name: 'Postgraduate Institute of Agriculture (PGIA)',
+                name: 'Postgraduate Institute of Science (PGIS)',
                 img: 'https://wallpapercave.com/wp/wp14925950.jpg',
                 link: '#',
             },
@@ -63,31 +63,31 @@ const programData = [
             },
             {
                 id: 5,
-                name: 'Postgraduate Institute of Science (PGIS)',
+                name: "Postgraduate Programs in Faculty of Allied Health Sciences",
                 img: 'https://wallpapercave.com/wp/wp12624870.jpg',
                 link: '#',
             },
             {
                 id: 6,
-                name: 'MSc. in Nursing from Faculty of Allied Health Sciences',
+                name: 'Postgraduate Programs in Faculty of Dental Sciences',
                 img: 'https://wallpapercave.com/wp/wp4907573.jpg',
                 link: '#',
             },
             {
                 id: 7,
-                name: 'MSc in Energy for Circular Economy',
+                name: 'Postgraduate Programs in Faculty of Engineering',
                 img: 'https://wallpapercave.com/wp/wp2172545.jpg',
                 link: '#',
             },
             {
                 id: 8,
-                name: 'MSc. in Nursing from Faculty of Allied Health Sciences',
+                name: 'Postgraduate Programs in Faculty of Management',
                 img: 'https://wallpapercave.com/wp/wp4907573.jpg',
                 link: '#',
             },
             {
                 id: 9,
-                name: 'MSc in Energy for Circular Economy',
+                name: 'Postgraduate Programs in Faculty of Veterinary Medicine and Animal Science',
                 img: 'https://wallpapercave.com/wp/wp2172545.jpg',
                 link: '#',
             }
